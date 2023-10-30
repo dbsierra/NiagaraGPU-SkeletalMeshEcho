19cfb1b1dcdae60577fdf8423f8bd41a92484a8b
dbsierra <dsierra4444@gmail.com>
2023-10-29 19:37:56 -0700

    Porting over assets from local project

---
+++ Unreal/Source/Unreal/Unreal.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Unreal.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Unreal, "Unreal" );
---
+++ Unreal/Source/Unreal/UnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_API AUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
